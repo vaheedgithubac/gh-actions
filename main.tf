ca74e7653da561224dc9e355c070f8bc8363c5d4
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2026-04-18 20:06:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,13 @@
+resource "aws_vpc" "vpc" {
+  cidr_block           = var.vpc_cidr
+  instance_tenancy     = "default"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
 
+  tags = merge(
+    var.common_tags,
+    {
+      Name = "${local.resource_name}-vpc-${var.vpc_count}"
+    }
+  )
+}
