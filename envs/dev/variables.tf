2ef3bf1da61804dfb4d720c4c12ae97606ce6b7e
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2026-04-18 21:03:28 +0530

    Update variables.tf

--- envs/dev/variables.tf
+++ envs/dev/variables.tf
@@ -1,4 +1,4 @@
-variable "region"       { default = "us-east-1" }
-variable "project_name" { default = "pharma" }
-variable "env"          { default = "dev" }
-variable "vpc_cidr"     { default  = "10.0.0.0/16" }
+variable "region"    { default = "us-east-1" }
+variable "project"   { default = "pharma" }
+variable "env"       { default = "dev" }
+variable "vpc_cidr"  { default  = "10.0.0.0/16" }
